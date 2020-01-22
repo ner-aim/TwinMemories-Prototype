97272ce756998a1f58f2c0191006a4db91e5c810
Pottapatri <40934305+ner-aim@users.noreply.github.com>
2020-01-22 22:04:06 +0530

    Player script

--- TwinMem/Assets/PlayerController.cs
+++ TwinMem/Assets/PlayerController.cs
@@ -14,6 +14,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                //Move player to what has been hit
+
+                //Stop focusing any objects
+            }
+        }
         
     }
 }
