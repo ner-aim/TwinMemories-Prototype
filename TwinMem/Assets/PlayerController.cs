9a89d1ffd419f66d0bf4fd419b52ab64aa3dbcb0
Pottapatri <40934305+ner-aim@users.noreply.github.com>
2020-01-22 21:55:03 +0530

    Added environment

--- TwinMem/Assets/PlayerController.cs
+++ TwinMem/Assets/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
